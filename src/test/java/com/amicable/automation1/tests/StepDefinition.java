895b215e5aec86b6b0e3480d215ed1b70c41bb7e
VINCENT <vinamic@yahoo.com>
2023-01-26 16:05:48 +0000

    HomeScreen code working fine

--- src/test/java/com/amicable/automation1/tests/StepDefinition.java
+++ src/test/java/com/amicable/automation1/tests/StepDefinition.java
@@ -3,6 +3,7 @@ package com.amicable.automation1.tests;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,48 +15,39 @@ import java.util.Map;
 public class StepDefinition {
     WebDriver driver;
 
-    @Given("User is logged in and on Contact us page")
-    public void user_is_logged_in_and_on_contact_us_page() {
+    @Given("User is logged in and on home Screen")
+    public void user_is_logged_in_and_on_home_screen() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://amaninja.co.uk/pages/contact");
+        driver.get("https://amaninja.co.uk/");
 
     }
-    @Then("Details should be filled in contact us form")
-    public void details_should_be_filled_in_contact_us_form(io.cucumber.datatable.DataTable dataTable) throws InterruptedException {
-
-
-        WebElement nameWE;
-        WebElement emailWE;
-        WebElement phoneNumberWE;
-        WebElement commentWE;
-        nameWE = driver.findElement(By.id("ContactForm-name"));
-        emailWE = driver.findElement(By.id("ContactForm-email"));
-        phoneNumberWE = driver.findElement(By.id("ContactForm-phone"));
-        commentWE = driver.findElement(By.id("ContactForm-body"));
-        List<Map<String, String>> contactUsInformation = dataTable.asMaps(String.class, String.class);
-        for (Map<String, String> i : contactUsInformation) {
-            if(!contactUsInformation.isEmpty()) {
-                String name = i.get("name");
-                nameWE.sendKeys(name);
-                String email = i.get("email");
-                emailWE.sendKeys(email);
-                String phoneNumber = i.get("phoneNumber");
-                phoneNumberWE.sendKeys(phoneNumber);
-                String comment = i.get("comment");
-                commentWE.sendKeys(comment);
-                Thread.sleep(3000);
-                nameWE.clear();
-                emailWE.clear();
-                phoneNumberWE.clear();
-                commentWE.clear();
-            }else{
-                System.out.println("Empty Datatable");
-            }
-
-
-        }
+    @Then("User should be able able to see {int} Product items")
+    public void user_should_be_able_able_to_see_product_items(Integer int1) {
 
     }
+    @Then("product items should have an following components")
+    public void product_items_should_have_an_following_components(io.cucumber.datatable.DataTable dataTable) {
 
-}
+        WebElement imageWE;
+        WebElement descriptionWE;
+        WebElement priceWE;
+        WebElement startbarWE;
+        WebElement chooseOptionButtonWE;
+        imageWE = driver.findElement(By.xpath("//*[@id=\"Slide-template--17083250704692__featured_collection-4\"]/div/div/div[1]/div[1]/div/img[1]"));
+        descriptionWE =  driver.findElement(By.xpath("//*[@id=\"CardLink-template--17083250704692__featured_collection-8067090841908\"]"));
+        priceWE =  driver.findElement(By.xpath("//*[@id=\"Slide-template--17083250704692__featured_collection-4\"]/div/div/div[2]/div[1]/div"));
+        startbarWE =  driver.findElement(By.xpath("//*[@id=\"Slide-template--17083250704692__featured_collection-4\"]/div/div/div[1]/div[2]/div[2]"));
+        chooseOptionButtonWE =  driver.findElement(By.xpath("//*[@id=\"Slide-template--17083250704692__featured_collection-4\"]/div/div/div[2]/div[2]"));
+
+        Assert.assertTrue(imageWE.isDisplayed());
+        Assert.assertTrue(descriptionWE.isDisplayed());
+        Assert.assertTrue(priceWE.isDisplayed());
+        Assert.assertTrue(startbarWE.isDisplayed());
+        Assert.assertTrue(chooseOptionButtonWE.isDisplayed());
+
+
+
+
+    }
+    }
